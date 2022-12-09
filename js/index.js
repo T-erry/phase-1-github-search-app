9153b432f60a4dbb99791b94cf0effcdf1db8c2e
T-erry <terrywachuka10@gmail.com>
2022-12-09 16:15:58 +0300

    completed assignment

---
+++ js/index.js
@@ -0,0 +1,57 @@
+ document.addEventListener("DOMContentLoaded", ()=>{
+    let form = document.getElementById("github-form")
+    form.addEventListener("submit", (e)=>{
+        e.preventDefault()
+        let input = document.getElementById("search").value
+        fetchNames(input);
+    })
+ })
+
+
+
+
+function fetchNames(users){
+    fetch(`https://api.github.com/users/${users}`)
+    .then((response)=> response.json())
+    .then((users)=> renderUsers(users))
+
+}
+function renderUsers(user){
+    console.log(user);
+    let ul = document.getElementById("user-list")
+    let li = document.createElement("li")
+    li.innerHTML = `
+      <a href = ${user.url}><img src =${user.avatar_url}/>user url</a>
+    <h3>${user.login}</h3>
+    <p style = "font-style: italic">${user.public_repos} repositories</p>
+    <button id="repositories">View Repositories</>
+    `
+    ul.appendChild(li);
+    let link = `https://api.github.com/users/${user.login}/repos`;
+    let repo = document.getElementById("repositories")
+        repo.addEventListener("click", ()=>{
+            repoLink(link)
+        })
+}
+        
+function repoLink(userRepos) {
+    fetch(userRepos)
+      .then((resp) => resp.json())
+      .then((names) => {
+        // console.log(data)
+        repoNames(names);
+      });
+  }
+  let clicked = true;
+  function repoNames(names) {
+    if(clicked){
+      names.forEach((name) => {
+        let repoContainer = document.getElementById("repos-list");
+        let li = document.createElement("li");
+        li.textContent = name.name;
+        repoContainer.appendChild(li);
+      });
+      clicked = false
+    }
+  }
+
